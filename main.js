b2254a384231a427fc170760604ae6725a1ab23a
化敏 <1906448030@qq.com>
2022-10-25 16:56:06 +0800

    '第一天完成'

--- main.js
+++ main.js
@@ -2,7 +2,29 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 导入请求包
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  // do somethimg...
+   wx.showLoading({
+      title: '数据加载中...',
+    })
+}
 
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  // do something...
+    wx.hideLoading()
+}
+uni.$showMsg= function(title='数据请求失败'){
+  uni.showToast({
+    title,
+    icon: 'none'
+  })
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,18 @@
 <template>
   <view>
-    我是home页面
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+      <swiper-item v-for="(item, index) in swiperList" :key="item.goods_id">
+        <navigator  class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+          <image :src="item.image_src" mode=""></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+    
+    <view class="nav-list">
+      <view class="nav-item" v-for="(item, index) in navList" :key="index" @click="navClick(item)">
+        <image :src="item.image_src" class="nav-img" mode=""></image>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +20,68 @@
   export default {
     data() {
       return {
-        
+        swiperList: [],
+        navList: [],
+        floorList:[]
       };
+    },
+    onLoad() {
+      this.getSwiperList()
+      this.getnavList()
+      this.getFloorList()
+    },
+    methods: {
+      async getSwiperList(){
+       const {data} = await uni.$http.get('/api/public/v1/home/swiperdata')
+       console.log(data);
+       if(data.meta.status!==200){
+         return uni.showToast()
+       }
+       this.swiperList=data.message
+      },
+      async getnavList(){
+       const {data} = await uni.$http.get('/api/public/v1/home/catitems')
+       console.log(data);
+       if(data.meta.status!==200){
+         return uni.showToast()
+       }
+       this.navList=data.message
+      },
+      navClick(item){
+        if(item.name === '分类'){
+          uni.switchTab({
+            url:"/pages/cate/cate"
+          })
+        }
+      },
+      async getFloorList(){
+        const {data}  = await uni.$http.get('/api/public/v1/home/floordata')
+        console.log(data);
+        if(data.meta.status!==200){
+          return uni.showToast()
+        }
+        this.floorList=data.message
+      }
     }
   }
 </script>
 
 <style lang="scss">
+swiper{
+  height: 330rpx;
+}
+.swiper-item, image{
+  width: 100%;
+  height: 100%;
+}
 
+.nav-list{
+  display: flex;
+  justify-content: space-around;
+  margin: 20px 0;
+  .nav-img{
+    width: 128rpx;
+    height: 140rpx;
+  }
+}
 </style>
