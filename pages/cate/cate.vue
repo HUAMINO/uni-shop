172ac8a57f6eefbdba7fbafdd256335305d17c2d
化敏 <1906448030@qq.com>
2022-10-25 11:22:34 +0800

    初始化项目

---
+++ pages/cate/cate.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    我是cate页面
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
